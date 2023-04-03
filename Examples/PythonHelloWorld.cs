1a6515de6c457e29701d368fe8c8ff9738a21192
张修睿 Cardidi <cardidi@live.cn>
2023-04-03 20:39:25 +0800

    Replace IronPython from 2.7.10 to 3.4

--- Examples/PythonHelloWorld.cs
+++ Examples/PythonHelloWorld.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using IronPython.Hosting;
 
 namespace Exodrifter.UnityPython.Examples
--- Examples/PythonLibrary.cs
+++ Examples/PythonLibrary.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using IronPython.Hosting;
 using IronPython.Runtime;
 
